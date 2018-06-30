4d1abd9741c68dfa005daea0cd662edc487ab150
Michael Overton <m.overtonbrown@gmail.com>
2018-06-30 14:16:11 -0700

    temporarily add config file to the git repo to test deploy

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+config.twitter = {
+	"consumerKey": "4aONsAS8x7bShvflBeHdkXOKH",
+	"consumerSecret": "sw0Z9XUnhBhPB9eaj6czi1jDXMdl9gI6QaiOvVJBlEobafqMBN",
+	"accessToken": "766041800-mfzJMJPm6AsVLKmXgi2ThSrlxIbOQiZ1RiM1gAbz",
+	"accessTokenSecret": "YdJSgghgsX9JLFSXgd6lXmT6fUWynkv8algBer2y1Lrxy",
+};
+config.ibm = {
+	"username": "0efe4426-f32e-4505-9131-f8b1f478e0c5",
+	"password": "aPhuLL1uT1oV",
+	"version_date": "2018-02-09"
+};
+module.exports = config;
